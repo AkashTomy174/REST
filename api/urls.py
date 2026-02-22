1cb012a1e19770cfc1b47462b2f8b11551c33eef
akash <akashtomy174@gmail.com>
2026-02-22 20:55:48 +0530

    ViewSet

--- api/urls.py
+++ api/urls.py
@@ -1,8 +1,18 @@
-from rest.views import home,person,persons
-from django.urls import path
+from rest.views import home,PeopleViewSet#,person,persons
+from django.urls import path,include
+
+from rest_framework.routers import DefaultRouter
+
+router = DefaultRouter()
+router.register('people', PeopleViewSet )
+
+
+
+
 
 urlpatterns=[
+    path('',include(router.urls)),
     path('home/',home),
-    path('person/',person),
-    path('persons/',persons.as_view())
+    # path('person/',person),
+    # path('persons/',Persons.as_view())
 ]
